1a9144e8ec4d3161a205476653e313e9398c9add
AnastasiiaBelianskaia <anastasiiabelianskaia@gmail.com>
2023-02-17 13:40:39 +0200

    Add parse cookie

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ if __name__ == '__main__':
 
 
 def parse_cookie(query: str) -> dict:
-    return {}
+    cookie_to_dict={}
+    pairs = query.split(';')
+    for pair in pairs:
+        if '=' in pair:
+            pair = pair.split('=', maxsplit=1)
+            cookie_to_dict[pair[0]] = pair[1]
+    return cookie_to_dict
 
 
 if __name__ == '__main__':
@@ -19,3 +25,13 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('name=') == {'name': ''}
+    assert parse_cookie('name') == {}
+    assert parse_cookie('=') == {'': ''}
+    assert parse_cookie('name===Dima;age=28;') == {'name': '==Dima', 'age': '28'}
+    assert parse_cookie('name==;') == {'name': '='}
+    assert parse_cookie('name;=Dima;') == {'': 'Dima'}
+    assert parse_cookie('name=Dima;name=Olga') == {'name': 'Olga'}
+    assert parse_cookie('name=Dima;age=28;id=0000;') == {'name': 'Dima', 'age': '28', 'id': '0000'}
+    assert parse_cookie('=;=') == {'': ''}
+    assert parse_cookie(';name=Dima;') == {'name': 'Dima'}
